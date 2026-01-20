eb3332c6f085f65e1a4593a640099365beffa30a
Your Name <your.email@example.com>
2026-01-20 12:42:08 +0530

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/sharebyte/SharebyteApplication.java
@@ -0,0 +1,13 @@
+package com.sharebyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharebyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharebyteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharebyte/SharebyteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharebyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharebyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
